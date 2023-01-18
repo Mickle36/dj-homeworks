f051fef47c27c4260d7c86b5f62d69fe6761e4ba
Mickle36 <maltsev0492@list.ru>
2023-01-18 16:27:41 +1100

    Добавил структуру функции bus_stations. Реализовал пагинацию. Изменил структуру index.html

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,5 +1,11 @@
+import pandas as pd
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+
+
+df = pd.read_csv('data-398-2018-08-30.csv')
+CONTENT = [i for i in df[['Name', 'Street', 'District']].values]
 
 
 def index(request):
@@ -7,11 +13,12 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
+    page_number = int(request.GET.get('page', 1))
+    paginator = Paginator(CONTENT, 10)
+    page = paginator.get_page(page_number)
 
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': page,
+        'page': page,
     }
     return render(request, 'stations/index.html', context)
